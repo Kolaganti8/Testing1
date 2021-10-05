e5b69e29ce20409c80ee25ba60507f1cfb682a8a
Kolaganti8 <89938314+Kolaganti8@users.noreply.github.com>
2021-10-05 08:00:30 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,7 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.0"
-      region = "eu-west-2"
-    }
+      }
   }
   backend "s3" {
     bucket = "cfl-terraform-test"
